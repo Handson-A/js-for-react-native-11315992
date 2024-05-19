f605941a73d6d697c87e926d35d6ea963c38601a
Ayel-son <ayelgumhandson001@gmail.com>
2024-05-19 01:32:38 +0000

    Task 2 completed

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -15,3 +15,30 @@ let Nums =[2,3,4,5,6];
 let finalNums = processArray(Nums);
 
 console.log(finalNums);*/
+
+
+//Task 2
+function formatArrayStrings (stringArray, numberArray) {
+    if (stringArray.length !== numberArray.length) {
+      throw new Error ("please have a corresponging number of items in both arrays"); // throws an error if both arrays have unequal number of items
+    }
+      return stringArray.map((string, index) => {
+    if ([index] % 2 === 0) {
+      return string.toUpperCase(); // Capitalizes entire string if the index in total is even
+    } else {
+      return string.toLowerCase(); // Converts string to lowercase if the index in total is odd
+    }
+  });
+}
+
+
+//test:
+const namesArray = ["Kwame","Kasoa","Yaw","lion","yam"]
+let number = [2,3,4,5,7] // string has to be of equal length with number array length
+let finalNumber = processArray(number);
+const resultString = formatArrayStrings(namesArray, finalNumber);
+
+//console.log (finalNumber);
+//console.log(resultString);
+
+export { namesArray, resultString };
